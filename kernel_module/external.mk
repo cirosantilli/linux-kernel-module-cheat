991397dede193d90bfcfd93ea3975de994f6db48
Ciro Santilli <ciro.santilli@gmail.com>
2017-06-05 01:16:38 +0100

    Remove redundant kernel module build commands.
    They were rebuilding every time.
    
    Fix run find for no patches

--- kernel_module/external.mk
+++ kernel_module/external.mk
@@ -9,18 +9,10 @@ KERNEL_MODULE_SITE = $(BR2_EXTERNAL_KERNEL_MODULE_PATH)
 KERNEL_MODULE_SITE_METHOD = local
 
 define KERNEL_MODULE_BUILD_CMDS
-	$(MAKE) \
-	  -C '$(@D)' \
-	  ARCH='$(KERNEL_ARCH)' \
-	  CC='$(TARGET_CC)' \
-	  CROSS_COMPILE='$(TARGET_CROSS)' \
-	  LD='$(TARGET_LD)' \
-	  LINUX_DIR='$(LINUX_DIR)' \
-	  PWD='$(@D)'
+	$(MAKE) -C '$(@D)/test' CC="$(TARGET_CC)" LD="$(TARGET_LD)"
 endef
 
 define KERNEL_MODULE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0655 $(@D)/*.ko '$(TARGET_DIR)'
 	$(INSTALL) -D -m 0755 $(@D)/test/*.out '$(TARGET_DIR)'
 endef
 
