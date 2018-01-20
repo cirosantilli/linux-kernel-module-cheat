69eb98b5ab55aff8bb0708872eb1c8f3e94a6f71
Ciro Santilli <ciro.santilli@gmail.com>
2018-01-20 10:21:33 +0000

    Document debugfs directory created automatically

--- rootfs_overlay/debugfs.sh
+++ rootfs_overlay/debugfs.sh
@@ -3,6 +3,8 @@ set -ex
 mkdir -p /debugfs
 # We also added a fstab entry that mounts this under /sys/kernel/debug autmoatically.
 # That is the most common place to mount it.
+# The /sys/kernel/debug directory gets created automatically when debugfs is
+# compiled into the kernel, but it does not get mounted automatically.
 mount -t debugfs none /debugfs
 insmod /debugfs.ko
 cd /debugfs/lkmc_debugfs
