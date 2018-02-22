2c4a9476be82f958527b43d67299c11c7ec15093
Ciro Santilli <ciro.santilli@gmail.com>
2018-02-22 00:23:57 +0000

    Fix x86 build with GEM5. Boot still fails as before.
    Use ./run -g instead of ./rungem5.
    
    More convenient GEM5 kernel command line options with -e differentially.

--- gem5/external.mk
+++ gem5/external.mk
@@ -8,8 +8,14 @@ GEM5_VERSION = 1.0
 GEM5_SITE = $(BR2_EXTERNAL_GEM5_PATH)
 GEM5_SITE_METHOD = local
 
+ifeq ($(ARCH),x86_64)
+ARCH_MAKE = x86
+else
+ARCH_MAKE = $(ARCH)
+endif
+
 define GEM5_BUILD_CMDS
-	cd '$(@D)/gem5/util/m5' && $(MAKE) -f 'Makefile.$(ARCH)' CC='$(TARGET_CC)' LD='$(TARGET_LD)'
+	cd '$(@D)/gem5/util/m5' && $(MAKE) -f 'Makefile.$(ARCH_MAKE)' CC='$(TARGET_CC)' LD='$(TARGET_LD)'
 endef
 
 define GEM5_INSTALL_TARGET_CMDS
