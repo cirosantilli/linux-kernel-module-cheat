2eff007f7c3458be240c673c32bb33892a45d3a0
Ciro Santilli <ciro.santilli@gmail.com>
2018-02-25 10:37:54 +0000

    GEM5 checkpoint switch to HPI for benchmarking.
    Don't pass -e on checkpoint restore.
    
    Add benchmarks to how much GEM5 is slower than QEMU.
    
    Rename Kernel boot command line arguments to match kernel docs name.
    
    Document how to pass extra options to GEM5.
    
    Start listing interesting benchmarks to run on GEM5.
    
    Add an openmp hello world.

---
+++ kernel_module/user/openmp.c
@@ -0,0 +1,17 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int nthreads, tid;
+#pragma omp parallel private(nthreads, tid)
+	{
+		tid = omp_get_thread_num();
+		printf("Hello World from thread = %d\n", tid);
+		if (tid == 0) {
+			nthreads = omp_get_num_threads();
+			printf("Number of threads = %d\n", nthreads);
+		}
+	}
+	return EXIT_SUCCESS;
+}
