a033e6444c9baf03f2db1c5493f537baa59cb070
Ciro Santilli <ciro.santilli@gmail.com>
2018-03-21 16:44:19 +0000

    parsec: move test.sh into the parsec repo

--- parsec-benchmark/external.mk
+++ parsec-benchmark/external.mk
@@ -16,6 +16,8 @@ define PARSEC_BENCHMARK_BUILD_CMDS
   # We run the benchmarks with the wrong arch here to generate the inputs on the host.
   # This is because on gem5 this takes too long to do.
   cd $(@D) && . env.sh && for pkg in $(BR2_PACKAGE_PARSEC_BENCHMARK_BUILD_LIST); do \
+    export GNU_TARGET_NAME='$(GNU_TARGET_NAME)'; \
+    export GNU_HOST_NAME='$(GNU_HOST_NAME)'; \
     export HOSTCC='$(HOSTCC)'; \
     export M4='$(HOST_DIR)/usr/bin/m4'; \
     export MAKE='$(MAKE)'; \
@@ -23,7 +25,7 @@ define PARSEC_BENCHMARK_BUILD_CMDS
     export TARGET_CROSS='$(TARGET_CROSS)'; \
     export HOSTTYPE='$(BR2_ARCH)'; \
     parsecmgmt -a build -p $$pkg; \
-  	if [ ! '$(BR2_PACKAGE_PARSEC_BENCHMARK_PARSECMGMT)' = y ]; then \
+    if [ ! '$(BR2_PACKAGE_PARSEC_BENCHMARK_PARSECMGMT)' = y ]; then \
       parsecmgmt -a run -p $$pkg -i $(BR2_PACKAGE_PARSEC_BENCHMARK_INPUT_SIZE); \
     fi \
   done
@@ -31,7 +33,7 @@ endef
 
 define PARSEC_BENCHMARK_INSTALL_TARGET_CMDS
   mkdir -p '$(TARGET_DIR)/parsec/'
-  $(INSTALL) -D -m 0755 '$(PARSEC_BENCHMARK_PKGDIR)/test.sh' '$(TARGET_DIR)/parsec/'
+  $(INSTALL) -D -m 0755 '$(D)/test.sh' '$(TARGET_DIR)/parsec/'
   if [ '$(BR2_PACKAGE_PARSEC_BENCHMARK_PARSECMGMT)' = y ]; then \
     rsync -am $(if $(filter $(V),1),-v,) \
       --exclude '**/obj/' \
