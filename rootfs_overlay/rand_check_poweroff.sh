2656202dff2e128a82113839ecb41bab03179d66
Ciro Santilli <ciro.santilli@gmail.com>
2018-03-25 19:01:24 +0100

    init: disable all userland processes started from init
    In order to make the system easier to understand.
    
    These include:
    
    * networking
    * klogd and syslogd. TODO what are those for? I could not see anything useful that they do.
    
    Also get rid of the useless S20random thing while we are at it.

--- rootfs_overlay/rand_check_poweroff.sh
+++ rootfs_overlay/rand_check_poweroff.sh
@@ -3,16 +3,8 @@ set -ex
 /rand_check.out
 
 # Check if network is being replayed.
-#
-# TODO; requires internet to be up, which is done by inittab.
-# We could do this with a /etc/init.d/SXX file,
-# but how to both check that into git, and make it optional?
-#
 # https://superuser.com/questions/635020/how-to-know-current-time-from-internet-from-command-line-in-linux
-# Raw IP because was failing with hostname:
-# https://unix.stackexchange.com/questions/124283/busybox-ping-ip-works-but-hostname-nslookup-fails-with-bad-address
 # https://tf.nist.gov/tf-cgi/servers.cgi
-#
 #echo | nc 129.6.15.28 13
 
 # busybox's poweroff panics, TODO why. Likely tries to kill shell.
