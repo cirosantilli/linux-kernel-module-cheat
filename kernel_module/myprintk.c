8a826ab16a72b1ecad0f5d0e95bd90f71e87b6cb
Ciro Santilli <ciro.santilli@gmail.com>
2018-05-05 21:42:47 +0100

    pr_debug: fix wrong insmod assertions, document mess of printk(KERN_DEBUG

--- kernel_module/myprintk.c
+++ kernel_module/myprintk.c
@@ -3,13 +3,13 @@
 
 static int myinit(void)
 {
-	pr_alert("printk alert\n");
-	pr_crit("printk crit\n");
-	pr_err("printk err\n");
-	pr_warning("printk warning\n");
-	pr_notice("printk notice\n");
-	pr_info("printk info\n");
-	pr_debug("printk debug\n");
+	pr_alert("pr_alert\n");
+	pr_crit("pr_crit\n");
+	pr_err("pr_err");
+	pr_warning("pr_warning\n");
+	pr_notice("pr_notice\n");
+	pr_info("pr_info\n");
+	pr_debug("pr_debug\n");
 	return 0;
 }
 
