149ab9302bea3f70e6a58136c52b05d22a93fa71
Ciro Santilli <ciro.santilli@gmail.com>
2018-05-05 09:09:12 +0100

    strlen_overflow: cleanup after reddit rush

--- kernel_module/strlen_overflow.c
+++ kernel_module/strlen_overflow.c
@@ -1,18 +1,14 @@
+/*
+https://www.reddit.com/r/hacking/comments/8h4qxk/what_a_buffer_overflow_in_the_linux_kernel_looks/
+*/
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/string.h>
 
 static int myinit(void)
 {
-	enum { size = 256 };
-	int i = 1;
-	char buf[size];
-	char buf2[size];
-	for (i = 0; i < size; i++) {
-		buf[i] = 'a';
-		buf2[i] = 'b';
-	}
-	/*char buf[] = {'p', 'w', 'n'};*/
+	char buf[] = {'p', 'w', 'n'};
 	pr_info("%llu\n", (long long unsigned)strlen(buf));
 	return 0;
 }
