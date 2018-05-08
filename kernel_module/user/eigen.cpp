a9a0b93d7c246348700455de734fe879836b0c66
Ciro Santilli <ciro.santilli@gmail.com>
2018-05-08 16:17:59 +0100

    eigen: add hello world
    Mention openmp on readme

---
+++ kernel_module/user/eigen.cpp
@@ -0,0 +1,14 @@
+/* Official hello world. */
+
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+using Eigen::MatrixXd;
+int main()
+{
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << m << std::endl;
+}
