a6a712acef4b6b150c801486a2fce05f9df5681d
Ciro Santilli <ciro.santilli@gmail.com>
2018-09-02 21:54:16 +0100

    init_env_poweroff: make into executable to reduce system noise

---
+++ kernel_module/user/init_env_poweroff.c
@@ -0,0 +1,26 @@
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <sys/reboot.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	puts("args:");
+	for (i = 0; i < argc; ++i)
+		puts(argv[i]);
+	puts("");
+
+	puts("env:");
+	extern char **environ;
+	char **env = environ;
+	while (*env) {
+		printf("%s\n", *env);
+		env++;
+	}
+	puts("");
+
+	/* Poweroff. */
+	reboot(RB_POWER_OFF);
+}
