c0de856222b572574915cab9f672fa810adb3745
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2018-11-14 10:10:12 +0000

    gdb test: set float

--- baremetal/arch/aarch64/regs.py
+++ baremetal/arch/aarch64/regs.py
@@ -1,6 +1,7 @@
 def test(self):
     self.sendline('tbreak main')
     self.sendline('continue')
+
     self.continue_to('x0')
     self.sendline('set $x0 = 3')
     self.continue_to('x1')
@@ -12,3 +13,15 @@ def test(self):
     assert self.get_int('$x30') == 3
     self.continue_to('x30')
     assert self.get_int('$x30') == 2
+
+    self.continue_to('d0')
+    self.sendline('set $d0 = 3.5')
+    self.continue_to('d1')
+    assert self.get_float('$d0') == 3.5
+    assert self.get_float('$d1') == 2.5
+    self.sendline('set $d31 = 3.5')
+    self.continue_to('d30')
+    assert self.get_float('$d30') == 1.5
+    assert self.get_float('$d31') == 3.5
+    self.continue_to('d31')
+    assert self.get_float('$d31') == 2.5
