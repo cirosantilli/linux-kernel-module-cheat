22e26aa0022023098648b791dfed03dcb19ede42
Ciro Santilli <ciro.santilli@gmail.com>
2019-01-22 00:00:00 +0000

    gem5-shell: fix after removal of --gem5

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def set_args(args, script_name):
     args['arch'] = 'aarch64'
-    args['gem5'] = True
+    args['emulators'] = ['gem5']
     if script_name == 'build-gem5':
         # This argument is defined only for ./build-gem5.
         args['extra_scons_args'] = ['ADSF=qwer']
