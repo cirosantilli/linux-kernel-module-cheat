23f80c231044dece7fee2b32fd069242f1e0e692
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2019-03-05 00:00:02 +0000

    binutils: describe gas hello world hack

--- userland/arch/x86_64/binutils_hack.c
+++ userland/arch/x86_64/binutils_hack.c
@@ -2,15 +2,17 @@
 #include <inttypes.h>
 
 int main(void) {
+#if 0
 	uint64_t in = 0xFFFFFFFF;
 	uint64_t out = 0;
 	__asm__ (
 		"mov %[in], %%rax;"
-		"inc %%rax;"
+		"myinc %%rax;"
 		"movq %%rax, %[out]"
 		: [out] "=g" (out)
 		: [in] "g" (in)
 		: "rax"
 	);
 	assert(out == in + 1);
+#endif
 }
