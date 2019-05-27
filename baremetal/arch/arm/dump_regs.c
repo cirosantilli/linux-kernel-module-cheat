24b75f92d866df48932259da73569740257085ef
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2019-05-27 00:00:04 +0000

    arm asm el: document virtualization failure after FP enable patch

--- baremetal/arch/arm/dump_regs.c
+++ baremetal/arch/arm/dump_regs.c
@@ -8,10 +8,19 @@ int main(void) {
     /* https://github.com/cirosantilli/linux-kernel-module-cheat#arm-exception-levels */
     printf("CPSR.M 0x%" PRIX32 "\n", cpsr & 0xF);
 
-    /* TODO this is blowing up an exception, how to I read from it? */
-    /*uint32_t mvfr1;*/
-    /*__asm__ ("vmrs %0, mvfr1" : "=r" (mvfr1) : :);*/
-    /*printf("MVFR1 0x%" PRIX32 "\n", mvfr1);*/
+#if 0
+    /* TODO blows up exception in EL, but works with -machine secure=on. */
+    uint32_t nsacr;
+    __asm__ ("mrc p15, 0, %0, c1, c1, 2" : "=r" (nsacr) : :);
+    printf("NSACR 0x%" PRIX32 "\n", nsacr);
+#endif
+
+#if 0
+    /* TODO blows up exception. */
+    uint32_t mvfr1;
+    __asm__ ("vmrs %0, mvfr1" : "=r" (mvfr1) : :);
+    printf("MVFR1 0x%" PRIX32 "\n", mvfr1);
+#endif
 
     return 0;
 }
