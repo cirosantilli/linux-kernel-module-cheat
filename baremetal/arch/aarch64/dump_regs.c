8eb312c58d585d2727e68b7917d9768ccf0a7ac4
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2019-05-27 00:00:01 +0000

    baremetal: move aarch64 el.c into dump_regs.c
    Also start disassembling registers nicely dump_regs so we can have a
    single executable to handle all register queries.

--- baremetal/arch/aarch64/dump_regs.c
+++ baremetal/arch/aarch64/dump_regs.c
@@ -6,6 +6,13 @@
 int main(void) {
     uint32_t sctlr_el1;
     __asm__ ("mrs %0, sctlr_el1" : "=r" (sctlr_el1) : :);
-    printf("sctlr_el1 0x%" PRIx32 "\n", sctlr_el1);
+    printf("SCTLR_EL1 0x%" PRIX32 "\n", sctlr_el1);
+    printf("SCTLR_EL1.A 0x%" PRIX32 "\n", (sctlr_el1 >> 1) & 1);
+
+    uint64_t CurrentEL;
+    __asm__ ("mrs %0, CurrentEL;" : "=r" (CurrentEL) : :);
+    printf("CurrentEL 0x%" PRIX64 "\n", CurrentEL);
+    /* https://github.com/cirosantilli/linux-kernel-module-cheat#arm-exception-levels */
+    printf("CurrentEL.EL 0x%" PRIX64 "\n", CurrentEL >> 2);
     return 0;
 }
--- baremetal/arch/arm/dump_regs.c
+++ baremetal/arch/arm/dump_regs.c
@@ -4,12 +4,14 @@
 int main(void) {
     uint32_t cpsr;
     __asm__ ("mrs %0, cpsr" : "=r" (cpsr) : :);
-    printf("cpsr  %" PRIx32 "\n", cpsr);
+    printf("CPSR 0x%" PRIX32 "\n", cpsr);
+    /* https://github.com/cirosantilli/linux-kernel-module-cheat#arm-exception-levels */
+    printf("CPSR.M 0x%" PRIX32 "\n", cpsr & 0xF);
 
     /* TODO this is blowing up an exception, how to I read from it? */
     /*uint32_t mvfr1;*/
     /*__asm__ ("vmrs %0, mvfr1" : "=r" (mvfr1) : :);*/
-    /*printf("mvfr1 %" PRIx32 "\n", mvfr1);*/
+    /*printf("MVFR1 0x%" PRIX32 "\n", mvfr1);*/
 
     return 0;
 }
