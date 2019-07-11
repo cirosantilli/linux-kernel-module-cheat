c0cc0bb6a700c9faf1a0fec6589588ca3671ec98
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2019-07-11 00:00:00 +0000

    m5ops.h: remove # from aarch64 literals

--- lkmc/m5ops.h
+++ lkmc/m5ops.h
@@ -42,17 +42,17 @@
 
 #elif defined(__aarch64__)
 
-#define LKMC_M5OPS_CHECKPOINT_ASM mov x0, #0; mov x1, #0; .inst 0xFF000110 | (0x43 << 16);
-#define LKMC_M5OPS_DUMPSTATS_ASM  mov x0, #0; mov x1, #0; .inst 0xFF000110 | (0x41 << 16);
-#define LKMC_M5OPS_EXIT_ASM       mov x0, #0;             .inst 0XFF000110 | (0x21 << 16);
-#define LKMC_M5OPS_FAIL_1_ASM     mov x0, #0; mov x1, #1; .inst 0xFF000110 | (0x22 << 16);
-#define LKMC_M5OPS_RESETSTATS_ASM mov x0, #0; mov x1, #0; .inst 0XFF000110 | (0x40 << 16);
+#define LKMC_M5OPS_CHECKPOINT_ASM mov x0, 0; mov x1, 0; .inst 0xFF000110 | (0x43 << 16);
+#define LKMC_M5OPS_DUMPSTATS_ASM  mov x0, 0; mov x1, 0; .inst 0xFF000110 | (0x41 << 16);
+#define LKMC_M5OPS_EXIT_ASM       mov x0, 0;            .inst 0XFF000110 | (0x21 << 16);
+#define LKMC_M5OPS_FAIL_1_ASM     mov x0, 0; mov x1, 1; .inst 0xFF000110 | (0x22 << 16);
+#define LKMC_M5OPS_RESETSTATS_ASM mov x0, 0; mov x1, 0; .inst 0XFF000110 | (0x40 << 16);
 
-#define LKMC_M5OPS_CHECKPOINT __asm__ __volatile__ ("mov x0, #0; mov x1, #0; .inst 0xFF000110 | (0x43 << 16);" : : : "x0", "x1")
-#define LKMC_M5OPS_DUMPSTATS  __asm__ __volatile__ ("mov x0, #0; mov x1, #0; .inst 0xFF000110 | (0x41 << 16);" : : : "x0", "x1")
-#define LKMC_M5OPS_EXIT       __asm__ __volatile__ ("mov x0, #0;             .inst 0XFF000110 | (0x21 << 16);" : : : "x0"      )
-#define LKMC_M5OPS_FAIL_1     __asm__ __volatile__ ("mov x0, #0; mov x1, #1; .inst 0xFF000110 | (0x22 << 16);" : : : "x0", "x1")
-#define LKMC_M5OPS_RESETSTATS __asm__ __volatile__ ("mov x0, #0; mov x1, #0; .inst 0XFF000110 | (0x40 << 16);" : : : "x0", "x1")
+#define LKMC_M5OPS_CHECKPOINT __asm__ __volatile__ ("mov x0, 0; mov x1, 0; .inst 0xFF000110 | (0x43 << 16);" : : : "x0", "x1")
+#define LKMC_M5OPS_DUMPSTATS  __asm__ __volatile__ ("mov x0, 0; mov x1, 0; .inst 0xFF000110 | (0x41 << 16);" : : : "x0", "x1")
+#define LKMC_M5OPS_EXIT       __asm__ __volatile__ ("mov x0, 0;            .inst 0XFF000110 | (0x21 << 16);" : : : "x0"      )
+#define LKMC_M5OPS_FAIL_1     __asm__ __volatile__ ("mov x0, 0; mov x1, 1; .inst 0xFF000110 | (0x22 << 16);" : : : "x0", "x1")
+#define LKMC_M5OPS_RESETSTATS __asm__ __volatile__ ("mov x0, 0; mov x1, 0; .inst 0XFF000110 | (0x40 << 16);" : : : "x0", "x1")
 
 #else
 #error m5ops not implemented for the current arch
