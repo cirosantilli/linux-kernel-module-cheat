c06923297fc0928a75efc63ffd8f49086327166b
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2019-10-29 00:00:00 +0000

    gem5 readfile use file, not stdin, or else sh does not work to start interaction

--- rootfs_overlay/lkmc/gem5.sh
+++ rootfs_overlay/lkmc/gem5.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # https://cirosantilli.com/linux-kernel-module-cheat#gem5-restore-new-script
 m5 checkpoint
+tmp="$(mktemp)"
+m5 readfile > "$tmp"
 m5 resetstats
-m5 readfile | sh
+sh "$tmp"
 m5 exit
