756ecea19525c218943439d630668b7cec1bfbd2
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2020-02-19 00:00:00 +0000

    pybind11: keep files in tree just for future reference, not properly automated yet

---
+++ userland/libs/pybind11/class_test_main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import class_test
+
+my_class_test = class_test.ClassTest("abc");
+print(my_class_test.getName())
+my_class_test.setName("012")
+print(my_class_test.getName())
