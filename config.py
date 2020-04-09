5d4349ad092d30644f963ed50bfca26ff7d13e85
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2020-04-09 01:00:00 +0000

    extra_scons_args -> extra_make_args in config.py example

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ def set_args(args, script_name):
     args['emulators'] = ['gem5']
     if script_name == 'build-gem5':
         # This argument is defined only for ./build-gem5.
-        args['extra_scons_args'] = ['ADSF=qwer']
+        args['extra_make_args'] = ['ADSF=qwer']
