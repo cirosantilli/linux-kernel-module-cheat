17ed296e2dd24cab8c7463b6937f75acc0d96f38
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2020-06-03 06:00:05 +0000

    boog -> boot

--- userland/linux/sysconf.c
+++ userland/linux/sysconf.c
@@ -9,7 +9,7 @@ int main(void) {
     /* Number of processors, not considering affinity:
      * http://stackoverflow.com/questions/2693948/how-do-i-retrieve-the-number-of-processors-on-c-linux */
     printf("_SC_NPROCESSORS_ONLN = %ld\n", sysconf(_SC_NPROCESSORS_ONLN));
-    /* CPUs configured by OS during boog. Some may have gone offline, so could be larger than _SC_NPROCESSORS_ONLN.a */
+    /* CPUs configured by OS during boot. Some may have gone offline, so could be larger than _SC_NPROCESSORS_ONLN.a */
     printf("_SC_NPROCESSORS_CONF = %ld\n", sysconf(_SC_NPROCESSORS_CONF));
     return EXIT_SUCCESS;
 }
