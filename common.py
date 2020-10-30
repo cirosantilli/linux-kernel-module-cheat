5c4683d3d3efa84db8186c957c6d4ce773f0360f
Ciro Santilli 六四事件 法轮功 <ciro.santilli@gmail.com>
2020-10-30 00:00:01 +0000

    linux: update to v5.9.2
    buildroot: update to 2020.08.1

--- common.py
+++ common.py
@@ -43,7 +43,7 @@ common = sys.modules[__name__]
 # Fixed parameters that don't depend on CLI arguments.
 consts = {}
 consts['repo_short_id'] = 'lkmc'
-consts['linux_kernel_version'] = '5.4.3'
+consts['linux_kernel_version'] = '5.9.1'
 # https://stackoverflow.com/questions/20010199/how-to-determine-if-a-process-runs-inside-lxc-docker
 consts['in_docker'] = os.path.exists('/.dockerenv')
 consts['root_dir'] = os.path.dirname(os.path.abspath(__file__))
